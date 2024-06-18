bb589e1fbe25380761aa2345cece625f707253dd
Danny Piper <djpiper28@gmail.com>
2024-06-18 16:22:13 +0100

    feat: added web server for checking for liveness

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import { Bot } from "./structs/Bot";
+import express from "express";
+
+const app = express();
+app.get("/", (_, res) => {
+  res.send("Healthy");
+});
+app.listen(3000);
 
 export const bot = new Bot(
   new Client({
